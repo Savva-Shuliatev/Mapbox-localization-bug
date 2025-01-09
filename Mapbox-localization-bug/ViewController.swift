80d969eedf6493ca309a8ee99b3e6fd1b5f3ed71
Savva <savva.shuliatev@yandex.ru>
2025-01-09 21:03:32 +0300

    Initial Commit

---
+++ Mapbox-localization-bug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mapbox-localization-bug
+//
+//  Created by Savva Shuliatev on 09.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
