8472965624c30314188b0e7331c36cc32af4d099
Savva <savva.shuliatev@yandex.ru>
2025-01-09 21:20:47 +0300

    Base test logic

--- Mapbox-localization-bug/ViewController.swift
+++ Mapbox-localization-bug/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import Combine
+import MapboxMaps
 
 class ViewController: UIViewController {
 
+  private lazy var mapView = MapView(frame: .zero)
+
+  private var cancellables: Set<AnyCancellable> = []
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    view.addSubview(mapView)
+    mapView.frame = view.bounds
+
+    preconditionFailure("Set your accessToken!")
+    MapboxOptions.accessToken = ""
+
+    mapView.mapboxMap.onMapLoaded.observe { [weak self] _ in
+      /// https://docs.mapbox.com/help/troubleshooting/change-language/#mapbox-maps-sdk-for-ios
+      try! self!.mapView.mapboxMap.localizeLabels(into: Locale(identifier: "ar"))
+    }
+    .store(in: &cancellables)
   }
 
 
