8f7c4d9dc4061cb597b817e430cad01b485f7b30
Savva <savva.shuliatev@yandex.ru>
2025-01-10 02:26:08 +0300

    Solution
    Reference to the blog describing localization support: https://www.mapbox.com/blog/maps-internationalization-34-languages

--- Mapbox-localization-bug/ViewController.swift
+++ Mapbox-localization-bug/ViewController.swift
@@ -26,10 +26,29 @@ class ViewController: UIViewController {
     mapView.mapboxMap.onMapLoaded.observe { [weak self] _ in
       /// https://docs.mapbox.com/help/troubleshooting/change-language/#mapbox-maps-sdk-for-ios
       try! self!.mapView.mapboxMap.localizeLabels(into: Locale(identifier: "ar"))
+      /// No effect
+
+      /// Undocumented Solution
+      // self!.localizeMap()
     }
     .store(in: &cancellables)
   }
 
+  /// Solution:
+  /// Sets up map localization according to the device's current language settings.
+  /// This utilizes an API that may not be officially documented.
+  /// Reference to the blog describing localization support:
+  /// https://www.mapbox.com/blog/maps-internationalization-34-languages
+  private func localizeMap() {
+    let locale = Locale.current
+    let settingsService = SettingsServiceFactory.getInstance(storageType: .persistent)
+    switch settingsService.set(key: MapboxCommonSettings.language, value: "\(locale)") {
+    case .success:
+      print("Successfully set MapboxCommonSettings.language \(locale)")
+    case .failure(let error):
+      assertionFailure("Failed to set MapboxCommonSettings.language with error: \(error)")
+    }
+  }
 
 }
 
